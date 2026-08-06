5bd33c61a37c4050e615033f99a72894e532f861
Eslezer <samuelfr05@hotmail.com>
2026-08-06 17:38:29 +1000

    establish Puzzle Trail !!!! identity and icon

---
+++ README.md
@@ -0,0 +1,5 @@
+# Puzzle Trail
+
+Puzzle Trail is a visual-maths adventure for Android. Players will solve picture puzzles, collect stars, and progress through a themed learning trail.
+
+This project is built with Kotlin and Jetpack Compose.
--- app/src/main/java/com/example/eduapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/eduapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.eduapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TrailPink = Color(0xFFD94D8B)
+val TrailPinkLight = Color(0xFFFFD9E7)
+val TrailViolet = Color(0xFF7652A6)
+val TrailVioletLight = Color(0xFFE9DFFF)
+val TrailGold = Color(0xFFFFB000)
+val TrailCream = Color(0xFFFFFBF4)
+val TrailNight = Color(0xFF101827)
